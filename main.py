f229a3571e9e366c66ac14bc14714fd16e68b81a
Subham Sarangi <subhamsarangi2016@gmail.com>
2017-12-07 15:32:41 +0530

    port

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ if __name__ == '__main__':
     conf = {
         'global': {
             'server.socket_host': '0.0.0.0',
-            'server.socket_port': int(os.environ.get('PORT', 5000)),
+            'server.socket_port': int(os.environ.get('url.port', 5000)),
         },
         #'/': {
         #    'tools.sessions.on': True,
